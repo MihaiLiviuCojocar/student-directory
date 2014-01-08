1c887049bb8738091aac1a61639af3ae68530ac0
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-08 14:23:13 +0000

    Variable added for student count

---
+++ directory.rb
@@ -0,0 +1,26 @@
+student_count = 16
+#first, we print the list of students
+puts "The students of my cohort at Makers Academy"
+puts "-------------------"
+puts "Berta Gutierrez"
+puts "Megan Folsom"
+puts "Rolando Barbella"
+puts "James Hunter"
+puts "Abraham Thomas"
+puts "Roy Gardiner"
+puts "Jorja Tracy Hung"
+puts "Colin Frankish"
+puts "Ayaz Goulamabasse"
+puts "Kalle Malmiharju"
+puts "Andrey Krahmal"
+puts "Biwek Shrestha"
+puts "Mihai-Liviu Cojocar"
+puts "Makis Otman"
+puts "Nabin Rai"
+puts "Peter Saxon"
+
+#finaly we print the total
+print "Overall we have "
+#it's important that print() doesn't add new line characters
+print student_count
+print " great students."
