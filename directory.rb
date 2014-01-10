5afa5b3a6461703af7153579eeb21873a519b029
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-10 17:19:03 +0000

    hashes added and changed print method

--- directory.rb
+++ directory.rb
@@ -2,22 +2,22 @@
 #let's put all the students into an array
 
 students = [ 
-	["Berta Gutierrez", :january],
- 	["Megan Folsom", :january],
- 	["Rolando Barbella", :january],
- 	["James Hunter", :january],
- 	["Abraham Thomas", :january],
- 	["Roy Gardiner", :january],
- 	["Jorja Tracy Hung", :january],
- 	["Colin Frankish", :january],
- 	["Ayaz Goulamabasse", :january],
- 	["Kalle Malmiharju", :january],
- 	["Andrey Krahmal", :january],
- 	["Biwek Shrestha", :january],
- 	["Mihai-Liviu Cojocar", :january],
- 	["Makis Otman", :january],
- 	["Nabin Rai", :january],
- 	["Peter Saxon", :january]
+	{:name => "Berta Gutierrez", :cohort => :january},
+ 	{:name => "Megan Folsom", :cohort => :january},
+ 	{:name => "Rolando Barbella", :cohort => :january},
+ 	{:name => "James Hunter", :cohort => :january},
+ 	{:name => "Abraham Thomas", :cohort => :january},
+ 	{:name => "Roy Gardiner", :cohort => :january},
+ 	{:name => "Jorja Tracy Hung", :cohort => :january},
+ 	{:name => "Colin Frankish", :name => :january},
+ 	{:name => "Ayaz Goulamabasse", :cohort => :january},
+ 	{:name => "Kalle Malmiharju", :cohort => :january},
+ 	{:name => "Andrey Krahmal", :cohort => :january},
+ 	{:name => "Biwek Shrestha", :cohort => :january},
+ 	{:name => "Mihai-Liviu Cojocar", :cohort => :january},
+ 	{:name => "Makis Otman", :cohort => :january},
+ 	{:name => "Nabin Rai", :cohort => :january},
+ 	{:name => "Peter Saxon", :cohort => :january}
 ]
 # and then we print them
 
@@ -26,9 +26,9 @@ def print_header
 	puts "-----------------------------"
 end
 
-def print(names)
-	names.each do |name|
-		puts name
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort})"
 	end
 end
 
