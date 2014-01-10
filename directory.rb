d04f2d92eb26dea846e4e0d8eb0902b1a59ca672
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-10 17:53:41 +0000

    added new method for students user input

--- directory.rb
+++ directory.rb
@@ -1,51 +1,39 @@
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
 
-#let's put all the students into an array
-
-students = [ 
-	{:name => "Berta Gutierrez", :cohort => :january},
- 	{:name => "Megan Folsom", :cohort => :january},
- 	{:name => "Rolando Barbella", :cohort => :january},
- 	{:name => "James Hunter", :cohort => :january},
- 	{:name => "Abraham Thomas", :cohort => :january},
- 	{:name => "Roy Gardiner", :cohort => :january},
- 	{:name => "Jorja Tracy Hung", :cohort => :january},
- 	{:name => "Colin Frankish", :name => :january},
- 	{:name => "Ayaz Goulamabasse", :cohort => :january},
- 	{:name => "Kalle Malmiharju", :cohort => :january},
- 	{:name => "Andrey Krahmal", :cohort => :january},
- 	{:name => "Biwek Shrestha", :cohort => :january},
- 	{:name => "Mihai-Liviu Cojocar", :cohort => :january},
- 	{:name => "Makis Otman", :cohort => :january},
- 	{:name => "Nabin Rai", :cohort => :january},
- 	{:name => "Peter Saxon", :cohort => :january}
-]
-# and then we print them
-
-def print_header
-	puts "The students of my cohort at Makers Academy"
-	puts "-----------------------------"
+	# create an empty array
+	students = []
+	# getthe first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do 
+		# add the student hash to the array
+		students << {:name => name, :cohort => :january}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return the array of students
+	students
 end
 
 def print(students)
-	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort})"
-	end
+   students.each do |student|
+     puts "#{student[:name]} (#{student[:cohort]} cohort})"
+   end
 end
+
+def print_header
+   puts "The students of my cohort at Makers Academy"
+   puts "-----------------------------"
+ end
 
 def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+   puts "Overall, we have #{names.length} great students"
 end
 
-# nothing happens until we call the methods
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
-
-
-
-
-
-
-
-
