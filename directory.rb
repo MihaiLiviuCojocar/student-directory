dcd063626f4c830fcaf036ce8d03d7e10ed81679
Mihai Liviu Cojocar <cmihailiviu@yahoo.com>
2014-01-14 15:38:51 +0000

    some methods changed - more tuned up

--- directory.rb
+++ directory.rb
@@ -1,3 +1,6 @@
+YES = "Y"
+NO = "N"
+
 def input_students
 	print "Please enter the names of the students\n"
 	print "To finish, just hit return twice\n"
@@ -23,14 +26,14 @@ def input_students
 		cohort_name = cohorts[cohort_num - 1]
 		puts "You entered -#{name}- and selected the -#{cohort_name}- cohort! Are you sure ? y/n "
 		# ask the user if sure about the choice
-		answer = ["Y","N"]
+		answer = [YES, NO]
 		user_answer = gets.capitalize.chomp
 		while !(answer).include? user_answer
 			puts "Please type y/n only!"
 			user_answer = gets.chomp.capitalize
 		end
 
-		if user_answer == answer[0]
+		if user_answer == YES
 			# after confirmation add the student hash to the array
 			students << {:name => name, :cohort => cohort_name}
 		else 
@@ -51,23 +54,19 @@ def input_students
 end
 
 def print_students(students)
-			i = 1
-			while i <= students.length
-				puts "#{i}. #{students[i-1][:name].center(30, "-")} (#{students[i-1][:cohort].center(9)} cohort)"
-				i = i + 1
-   			end
+	students.each.with_index(1) do |student, index|
+		puts "#{index}. #{student[:name].center(30, "-")} (#{student[:cohort].center(9)} cohort)"		
+	end
 end
 
 # method for printing the header
-def print_header(students)
-	if students.length != 0
-    	print "The students of my cohort at Makers Academy\n"
-    	print "-----------------------------\n"
-    end
+def print_header	
+	print "The students of my cohort at Makers Academy\n"
+	print "-----------------------------\n"
  end
 
 # added choice for how the list is sorted and printed
-def print_choice(students)
+def print_choice(students)  
 	puts "How do you want to print the student's list ? By name or by cohort ? n/c"
 	answer_choices = ["N","C"]
 	choice_answer = gets.capitalize.chomp
@@ -76,28 +75,24 @@ def print_choice(students)
 		puts "Please type 'n' or 'c' !"
 		choice_answer = gets.capitalize.chomp
 	end
-		puts print_header(students)
-		
-	if choice_answer == answer_choices[0]
-		sort_by_name = students.sort_by{ |name| name[:name]}
-		print_students(sort_by_name)
-	else
-		sort_by_cohort = students.sort_by{ |cohort| cohort[:cohort]}
-		print_students(sort_by_cohort)
-	end
+	puts print_header unless students.empty?
+	
+  sort_field = choice_answer == 'N' ? :name : :cohort
+  sorted = students.sort_by{|student| student[sort_field]}
+  print_students(sorted)
 end
 
 # method for printing the footer
 def print_footer(names)
 	if names.length == 0
 		puts "You didn't enter any name !! "
+	else
+		if names.length == 1
+			puts "Overall, we have just one great student."
 		else
-			if names.length == 1
-				puts "Overall, we have just one great student."
-				else
-    				print "Overall, we have #{names.length} great students. \n"
-    			end
-    end
+			print "Overall, we have #{names.length} great students. \n"
+		end
+  end
 end
 
 students = input_students
