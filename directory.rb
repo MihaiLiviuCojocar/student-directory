dd84aa20c4b6fe2c2066a26de9c2ed3ee6f9f712
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-02-02 23:01:55 +0000

    minor code refraction and some bugs fixed

--- directory.rb
+++ directory.rb
@@ -1,31 +1,41 @@
 YES = "Y"
-NO = "N"
+NO  = "N"
+@cohorts = ["January","February","March","April","May","June","July","August","September","Octomber","November","December"]
+@students = []
+
+def print_menu
+	puts "1. Input the students"
+ 	puts "2. Show the students"
+ 	puts "9. Exit"
+end
+
+def show_students
+	print_choice(@students)
+	print_footer(@students)
+end
+
+def selection_choice
+	selection = gets.chomp
+	case selection
+ 		when "1"
+ 			# input the students
+ 			@students = input_students	
+ 		when "2"
+ 			# show the students
+ 			show_students
+ 		when "9"
+ 			exit # exit
+ 		else puts "I don't know what you meant, try again!"
+ 		end
+end
 
 def interactive_menu
 	loop do
-	 # 1. print the menu and ask the user what to do
-	 	puts "1. Input the students"
-	 	puts "2. show the students"
-	 	puts "9. exit"
-
-	 # 2. read the input and save it into a variable
-	 	selection = gets.chomp
-	 # 3. do what the user has asked
-	 	case selection
-	 		when "1"
-	 			# input the students
-	 			@students = input_students	
-	 		when "2"
-	 			# show the students
-	 			print_choice(@students)
-				print_footer(@students)
-
-	 		when "9"
-	 			exit # exit
-	 		else 
-	 			puts "I don't know what you meant, try again!"
-	 		end
-
+		# 1. print the menu and ask the user what to do
+	 	print_menu
+		# 2. read the input and save it into a variable	
+		# 3. do what the user has asked
+		selection_choice
 	end
 end
 
@@ -33,26 +43,20 @@ end
 def input_students
 	print "Please enter the names of the students\n"
 	print "To finish, just hit return twice\n"
-
-	# create an empty array
-	@students = []
 	# get the first name
-	name = gets.delete("\n")
+	name = gets.chomp
 	# while the name is not empty, repeat this code
 	while !name.empty? do 
 		# we give the user the option to choose from
-		puts "Now enter the student's cohort: \n (Enter 1 for January, 2 for February, .... , 12 for December)"
-		cohorts = ["January","February","March","April","May","June","July","August","September","Octomber","November","December"]
-		cohort_num = gets.to_i
-		
+		puts "Now enter the student's cohort: \n (Enter 1 for January, 2 for February, .... , 12 for December)"		
+		cohort_num = gets.to_i		
 		# if we get a wrong input, repeat the code until succesful
-		while !(1..cohorts.size).include? cohort_num
-			puts "Please use a number between 1 and #{cohorts.size}:"
+		while !(1..@cohorts.size).include? cohort_num
+			puts "Please use a number between 1 and #{@cohorts.size}:"
 			cohort_num = gets.to_i
 		end
-
 		# after a succesful input, assign the cohort name
-		cohort_name = cohorts[cohort_num - 1]
+		cohort_name = @cohorts[cohort_num - 1]
 		puts "You entered -#{name}- and selected the -#{cohort_name}- cohort! Are you sure ? y/n "
 		# ask the user if sure about the choice
 		answer = [YES, NO]
@@ -61,14 +65,11 @@ def input_students
 			puts "Please type y/n only!"
 			user_answer = gets.chomp.capitalize
 		end
-
-		if user_answer == YES
-			# after confirmation add the student hash to the array
+		# after confirmation add the student hash to the array
+		if user_answer == YES 
 			@students << {:name => name, :cohort => cohort_name}
-		else 
-			puts "Chose again !"
 		end
-
+	
 		if @students.length == 1
 			print "Now we have one student. \n Enter the name of the next student \n"
 		else
@@ -91,7 +92,7 @@ end
 # method for printing the header
 def print_header	
 	print "The students of my cohort at Makers Academy\n"
-	print "-----------------------------\n"
+	print "-----------------------------------------------------\n"
  end
 
 # added choice for how the list is sorted and printed
@@ -104,8 +105,7 @@ def print_choice(students)
 		puts "Please type 'n' or 'c' !"
 		choice_answer = gets.capitalize.chomp
 	end
-	puts print_header unless students.empty?
-	
+	print_header unless students.empty?
   sort_field = choice_answer == 'N' ? :name : :cohort
   sorted = students.sort_by{|student| student[sort_field]}
   print_students(sorted)
@@ -113,14 +113,10 @@ end
 
 # method for printing the footer
 def print_footer(names)
-	if names.length == 0
-		puts "You didn't enter any name !! "
-	else
-		if names.length == 1
-			puts "Overall, we have just one great student."
-		else
-			print "Overall, we have #{names.length} great students. \n"
-		end
+	case names.length
+	when 0 then print_header; puts "The list is empty !! \n \n"
+	when 1 then puts "Overall, we have just one great student." 
+	else print "Overall, we have #{names.length} great students. \n"
   end
 end
 
